0538fe42d903263a7cb528000ff3e8ced5aff024
韩旭小天才 <905583741@qq.com>
2021-12-06 14:42:52 +0800

    fix:完善 系统监控 - 服务监控页面

--- src/views/monitor/server/index.tsx
+++ src/views/monitor/server/index.tsx
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-12-06 10:25:05
- * @LastEditTime: 2021-12-06 13:40:26
+ * @LastEditTime: 2021-12-06 14:37:04
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /use-hooks/src/views/monitor/server/index.tsx
@@ -34,7 +34,7 @@ function Server(props: any) {
   return (
     <>
       <Row>
-        <Col span={12} style={{ paddingRight: "30px" }}>
+        <Col span={12} style={{ paddingRight: "20px" }}>
           <Card title="CPU">
             <div className="ant-table">
               <table cellSpacing="0" style={{ width: "100%" }}>
@@ -74,7 +74,7 @@ function Server(props: any) {
             </div>
           </Card>
         </Col>
-        <Col span={12} style={{ paddingLeft: "30px" }}>
+        <Col span={12} style={{ paddingLeft: "20px" }}>
           <Card title="内存">
             <div className="ant-table">
               <table cellSpacing="0" style={{ width: "100%" }}>
@@ -115,6 +115,148 @@ function Server(props: any) {
                     <td>{server.jvm && <div style={{ color: server.jvm.usage > 80 ? "red" : "" }}>{server.jvm.usage}%</div>}</td>
                   </tr>
                 </tbody>
+              </table>
+            </div>
+          </Card>
+        </Col>
+      </Row>
+      <Row style={{ marginTop: 10 }}>
+        <Col span={24}>
+          <Card title="服务器信息">
+            <div className="ant-table">
+              <table cellSpacing="0" style={{ width: "100%" }}>
+                <tbody className="ant-table-tbody">
+                  <tr>
+                    <td>
+                      <div>服务器名称</div>
+                    </td>
+                    <td>{server.sys && <div>{server.sys.computerName}</div>}</td>
+                    <td>
+                      <div>操作系统</div>
+                    </td>
+                    <td>{server.sys && <div>{server.sys.osName}</div>}</td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <div>服务器IP</div>
+                    </td>
+                    <td>{server.sys && <div>{server.sys.computerIp}</div>}</td>
+                    <td>
+                      <div>系统架构</div>
+                    </td>
+                    <td>{server.sys && <div>{server.sys.osArch}</div>}</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </Card>
+        </Col>
+      </Row>
+      <Row style={{ marginTop: 10 }}>
+        <Col span={24}>
+          <Card title="Java虚拟机信息">
+            <div className="ant-table">
+              <table cellSpacing="0" style={{ width: "100%" }}>
+                <tbody className="ant-table-tbody">
+                  <tr>
+                    <td>
+                      <div>Java名称</div>
+                    </td>
+                    <td>{server.jvm && <div>{server.jvm.name}</div>}</td>
+                    <td>
+                      <div>Java版本</div>
+                    </td>
+                    <td>{server.jvm && <div>{server.jvm.version}</div>}</td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <div>启动时间</div>
+                    </td>
+                    <td>{server.jvm && <div>{server.jvm.startTime}</div>}</td>
+                    <td>
+                      <div>运行时长</div>
+                    </td>
+                    <td>{server.jvm && <div>{server.jvm.runTime}</div>}</td>
+                  </tr>
+                  <tr>
+                    <td colSpan={1}>
+                      <div>安装路径</div>
+                    </td>
+                    <td colSpan={3}>{server.jvm && <div>{server.jvm.home}</div>}</td>
+                  </tr>
+                  <tr>
+                    <td colSpan={1}>
+                      <div>项目路径</div>
+                    </td>
+                    <td colSpan={3}>{server.sys && <div>{server.sys.userDir}</div>}</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </Card>
+        </Col>
+      </Row>
+      <Row style={{ marginTop: 10 }}>
+        <Col span={24}>
+          <Card title="磁盘状态">
+            <div className="ant-table">
+              <table cellSpacing="0" style={{ width: "100%" }}>
+                <thead className="ant-table-thead">
+                  <tr>
+                    <th>
+                      <div>盘符路径</div>
+                    </th>
+                    <th>
+                      <div>文件系统</div>
+                    </th>
+                    <th>
+                      <div>盘符类型</div>
+                    </th>
+                    <th>
+                      <div>总大小</div>
+                    </th>
+                    <th>
+                      <div>可用大小</div>
+                    </th>
+                    <th>
+                      <div>已用大小</div>
+                    </th>
+                    <th>
+                      <div>已用百分比</div>
+                    </th>
+                  </tr>
+                </thead>
+                {server.sysFiles && (
+                  <tbody className="ant-table-tbody">
+                    {server.sysFiles.map((sysFile: any) => {
+                      return (
+                        <tr>
+                          <td>
+                            <div>{sysFile.dirName}</div>
+                          </td>
+                          <td>
+                            <div>{sysFile.sysTypeName}</div>
+                          </td>
+                          <td>
+                            <div>{sysFile.typeName}</div>
+                          </td>
+                          <td>
+                            <div>{sysFile.total}</div>
+                          </td>
+                          <td>
+                            <div>{sysFile.free}</div>
+                          </td>
+                          <td>
+                            <div>{sysFile.used}</div>
+                          </td>
+                          <td>
+                            <div style={{ color: server.mem.usage > 80 ? "red" : "" }}>{sysFile.usage}%</div>
+                          </td>
+                        </tr>
+                      );
+                    })}
+                  </tbody>
+                )}
               </table>
             </div>
           </Card>
