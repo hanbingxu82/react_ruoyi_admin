2bb6686a3946772db8b3794e9498d6d70bfe22d4
韩旭小天才 <905583741@qq.com>
2021-12-07 09:18:13 +0800

    fix:生命周期销毁 消除window.onresize方法

--- src/views/monitor/druid/index.tsx
+++ src/views/monitor/druid/index.tsx
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-12-06 15:42:24
- * @LastEditTime: 2021-12-06 17:06:50
+ * @LastEditTime: 2021-12-07 09:00:10
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /use-hooks/src/views/monitor/druid/index.tsx
@@ -21,6 +21,10 @@ function Druid(props: any) {
     setHeight(document.documentElement.clientHeight - 170 + "px");
     window.onresize = function temp() {
       setHeight(document.documentElement.clientHeight - 170 + "px");
+    };
+    return () => {
+    // 清除函数
+      window.onresize = null;
     };
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
